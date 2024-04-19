8d8af26ba6b33a84d7aea9ced12cd98081fa459c
mayihua <mayihua2005@126.com>
2024-04-19 17:04:23 +0800

    bat added

--- start.bat
+++ start.bat
@@ -1,8 +1,6 @@
 @echo off
 if not exist .\build (mkdir .\build)
 
-if not exist .\build\snake.love (
-  zip -q -r .\build\snake.zip .\ui .\game .\game.lua .\main.lua .\const.lua .\images .\audios
-  cd .\build
-  ren snake.zip snake.love
-)
+zip -q -r .\build\snake.zip .\ui .\game .\game.lua .\main.lua .\const.lua .\images .\audios
+cd .\build
+ren snake.zip snake.love
